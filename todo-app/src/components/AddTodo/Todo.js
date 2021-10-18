5825d7507d9576e4219df4f5c5262926a85f9282
ttphuongvi <4501104276@student.hcmue.edu.vn>
2021-10-19 01:13:36 +0700

    Todo Add new task

--- todo-app/src/components/AddTodo/Todo.js
+++ todo-app/src/components/AddTodo/Todo.js
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import './Todo.css'
 const Todo = () => {
     return (
-        <nav>
-            <p>P</p>
-        </nav>
+        <form className="form-add-todo">
+            <section className="form-input">
+                <label>New Task:</label>
+                <div class="input">
+                    <input className="effect-8" type="text" placeholder="Placeholder Text"/>
+                        <span className="focus-border">
+                        <i></i>
+                        </span>
+                </div>
+            </section>
+            <section className="form-input">
+                <label>Deadline:</label>
+                <div class="input">
+                    <input className="effect-8" type="text" placeholder="Placeholder Text"/>
+                        <span className="focus-border">
+                        <i></i>
+                        </span>
+                </div>
+            </section>
+            <button className=" custom-btn add-new-task">ADD</button>
+        </form>
     )
 }
 
--- todo-app/src/components/Header/Header.js
+++ todo-app/src/components/Header/Header.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import './Header.css'
-import {Link, Switch, BrowserRouter, Route} from 'react-router-dom'
-import Todo from '../AddTodo/Todo'
 function Header() {
   return (
-    <BrowserRouter>
     <header className="header">
-          <nav>
-            <Link to="/Todo-App" className="logo">TODO LIST</Link>
-          </nav>
+            <p to="/Todo-App" className="logo">TODO APP</p>
           <a href="https://github.com/ttphuongvi/Todo-App" target="_blank" className="profile">TRỊNH THỊ THỊ PHƯƠNG VI  |  4501104276</a>
         </header>
-        <Switch>
-          <Route path="/Todo-App"><Todo/></Route>
-        </Switch>
-        </BrowserRouter>
   )
 }
 
