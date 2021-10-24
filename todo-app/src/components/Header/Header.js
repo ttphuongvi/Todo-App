ad456a98aa4a678b978845751c0c89129014266d
ttphuongvi <4501104276@student.hcmue.edu.vn>
2021-10-24 18:46:21 +0700

    edit

--- todo-app/src/components/Header/Header.js
+++ todo-app/src/components/Header/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="header">
             <p to="/Todo-App" className="logo">TODO APP</p>
-          <a href="https://github.com/ttphuongvi/Todo-App" target="_blank" className="profile">TRỊNH THỊ THỊ PHƯƠNG VI  |  4501104276</a>
+          <a href="" target="_blank" className="profile">XIN CHÀO BẠN!</a>
         </header>
   )
 }
